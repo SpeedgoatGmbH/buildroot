1d5bd5c8756b26c1caaff321fb3388c411fbdc2d
bvongunten <brian.vongunten@speedgoat.ch>
2019-01-17 16:38:38 +0000

    Adding new entry for the speedgoat hardware.

--- package/aravis/aravis.mk
+++ package/aravis/aravis.mk
@@ -1,33 +1,20 @@
-#######################################################################
+################################################################################
 #
 # libaravis
 #
-#######################################################################
-LIBARAVIS_VERSION = 0.6.0
-LIBARAVIS_SOURCE =  aravis-$(LIBARAVIS_VERSION).tar.xz 
-LIBARAVIS_SITE = http://ftp.gnome.org/pub/GNOME/sources/aravis/0.6/
-LIBARAVIS_LICENSE =  LGPL v2+
-LIBARAVIS_LICENSE_FILES = COPYING
-LIBARAVIS_INSTALL_STAGING = YES
+################################################################################
 
-LIBARAVIS_DEPENDENCIES = libxml2 libglib2 host-pkgconf bar gstreamer audit libusb
+ARAVIS_VERSION = 0.6.0
+ARAVIS_SOURCE =  aravis-$(ARAVIS_VERSION).tar.xz
+ARAVIS_SITE = http://ftp.gnome.org/pub/GNOME/sources/aravis/0.6
+ARAVIS_INSTALL_STAGING = YES
+ARAVIS_DEPENDENCIES = libxml2 libglib2 gstreamer audit libusb
+ARAVIS_CONF_OPTS = --enable-viewer=false
+ARAVIS_CONF_OPTS += --enable-gst-0.1-plugin=true
+ARAVIS_CONF_OPTS += --enable-gst-plugin=false
+ARAVIS_CONF_OPTS += --enable-packet-socket=true
+ARAVIS_CONF_OPTS += --enable-usb=false
+ARAVIS_CONF_OPTS += --enable-fast-heartbeat=false
+ARAVIS_CONF_OPTS += --enable-gtk-doc-html=false
 
-LIBARAVIS_CONF_OPTS += -gst-plugin=true
-LIBARAVIS_CONF_OPTS += -packet-socket=true 
-LIBARAVIS_CONF_OPTS += -usb=true 
-LIBARAVIS_CONF_OPTS += -fast-heartbeat=true
-LIBARAVIS_CONF_OPTS += -viewer=false
-LIBARAVIS_CONF_OPTS += -documentation=false
-LIBARAVIS_CONF_OPTS += -introspection=false
-
-
-$(eval $(meson-package))
-
-#option('gst-plugin', type: 'boolean', value: true,
-#  description : 'Build GStreamer plugin')
-#option('packet-socket', type: 'boolean', value: true,
-#  description : 'Enable packet socket support')
-#option('usb', type: 'boolean', value: true,
-#  description : 'Enable USB support')
-#option('fast-heartbeat', type: 'boolean', value: false,
-#description: 'Enable faster heartbeat rate')
+$(eval $(autotools-package))
